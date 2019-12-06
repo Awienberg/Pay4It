5c39a435eafdf220fec33e3a47e9e30974c4f40b
Awienberg <a.wienberg@hotmail.com>
2019-12-06 14:00:19 +0100

    api

--- main.js
+++ main.js
@@ -2,25 +2,54 @@
 import {$} from "./modules/nQuery.js";
 import {Ajax} from "./modules/Ajax.js";
 
-//Event handler for fortune button - tests responseText
-let getNewContent = function() {
-    let req = Object.create(Ajax);
-    req.init();
-    req.getFile("", txtHandler);
-}
+let xhr = new XMLHttpRequest()
 
-//ajax load event puts received text onto the dom into the dom
-let txtHandler = function(e) {
-    let obj = JSON.parse(e.target.responseText);    // objectify response
-                                                    // and use it
-}
+xhr.open("GET", "https://consolwebapi.pay4it.dk/api/Devices/Detail?deviceID=562");
+xhr.setRequestHeader("Authorization", "Basic cmlra2VfY2FybHNzb25AbGl2ZS5kazoJIDRqUE1kTmg2Nw==");
+xhr.setRequestHeader("Cache-Control", "no-cache");
 
-//Click button
-let showStarter = function () {
-    $("solarie1").addEventListener("click", getNewContent);
-}
+xhr.send()
 
-window.addEventListener("load", showStarter);
+xhr.addEventListener("load", function(){
+  let json = xhr.responseText;
+  json = JSON.parse(json)
+  console.log(json);
+
+  let booths = json.Booths;
+  let shelves = json.Shelves;
+
+  //EACH BOOTH
+  for (let booth of booths) {
+    console.log(booth.Description);
+    console.log(booth);
+  }
+
+  //EACH SHELVE
+  for (let shelve of shelves) {
+    console.log(shelve.Description);
+    console.log(shelve);
+  }
+});
+
+/*
+//loop that prints the data from server into the HTML
+for (let i = 0; i < obj.length; i++) {
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let id = document.createTextNode(`${obj[i].id}`);
+    td1.appendChild(id);
+    let td2 = document.createElement("td");
+    let city = document.createTextNode(`${obj[i].name}`);
+    td2.appendChild(city);
+    let td3 = document.createElement("td");
+    let population = document.createTextNode(`${obj[i].population}`);
+    td3.appendChild(population);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    $("new").appendChild(tr);
+    }
+*/
 
 //When .kategori is clicked, toggle css .large
 let arr = document.getElementsByClassName("kategori");
